0fe5c5da99cfaf0b9d130adb94886beecfbfe7dd
mor mbathie <mormbathie98@gmail.com>
2026-04-13 19:47:45 +0100

    ajoute du configuration de jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,25 @@ import { UsersService } from 'src/users/users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constant';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        global: true,
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1h' },
+
+      })
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService,UsersService]
+  providers: [AuthService, UsersService]
 })
-export class AuthModule {}
+export class AuthModule { }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,10 +2,14 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { AuthModelDto } from './dto/auth.model.dto';
 import { compare } from 'bcrypt';
+import { JwtService } from '@nestjs/jwt/dist/jwt.service';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly userService: UsersService) { }
+    constructor(
+        private readonly userService: UsersService,
+        private readonly jwtService: JwtService
+    ) { }
 
     async login(authModelDto: AuthModelDto) {
         const { username, password } = authModelDto;
@@ -13,12 +17,20 @@ export class AuthService {
         if (!existingUser) throw new NotFoundException("username or password is incorrect");
         const isPasswordValid = await this.validatePassword(password, existingUser.password);
         if (!isPasswordValid) throw new NotFoundException("username or password is incorrect");
-        return {existingUser, message: "login successful"};
+        return this.Authentification({id: existingUser.id});
 
     }
 
     private async validatePassword(password: string, hashPassword: string): Promise<boolean> {
         return await compare(password, hashPassword);
+    }
+
+
+    private async Authentification({id}: {id: number}){
+
+        const payload = {id};
+        return {access_token: await this.jwtService.sign(payload)}
+
     }
 
 }
